26125b896ba49fe891c3ec7155be42c5083e4e29
wizardy <wizardhassan99@gmail.com>
2025-01-03 20:32:24 +0100

    commit

--- src/main/java/OneToMany/Adresse.java
+++ src/main/java/OneToMany/Adresse.java
@@ -4,13 +4,14 @@ import javax.persistence.*;
 @Entity // L'entité sera mappée à une table dans la base de données.
 public class Adresse {
   @Id // Déclare cette propriété comme étant l'identifiant de l'entité.
-  @GeneratedValue(strategy = GenerationType.IDENTITY) // L'ID sera généré automatiquement (auto-incrémenté) par la base de données.
+  @GeneratedValue(strategy = GenerationType.IDENTITY) 
   private Long id; // L'ID de l'adresse.
   private String rue; // L'attribut représentant la rue de l'adresse.
   // Relation ManyToOne : Chaque adresse est associée à un seul département.
   @ManyToOne 
   @JoinColumn(name = "departement_id") // Spécifie la colonne dans la table Adresse qui fera référence à l'entité Departement.
   private Departement departement; // L'entité Departement associée à cette adresse.
+  
   // Getter et Setter pour l'ID.
   public Long getId() { return id; }
   public void setId(Long id) { this.id = id; }
